fa079ffd30244541721623157a6693a436688657
Melinda Kiss <savarosa@protonmail.com>
2026-03-05 17:30:43 -0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TattingDesigner from './tattingindex.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TattingDesigner />
+  </React.StrictMode>
+);
